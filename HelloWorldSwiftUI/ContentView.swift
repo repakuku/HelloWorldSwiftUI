4b883d3df040d3d95ccad6a4e33876068a399a5a
Alexey Turulin <repakuku@icloud.com>
2023-09-06 16:52:43 -0400

    working with content view

--- HelloWorldSwiftUI/ContentView.swift
+++ HelloWorldSwiftUI/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello, SwiftUI!")
-                .font(.title)
-                .foregroundColor(.red)
-            HStack {
-                Text("This is SwiftUI View")
-                    .font(.subheadline)
-                Spacer()
-                Text("Lesson 1")
+        VStack {
+            SwiftLogoView(color: .orange)
+                .padding(.bottom, -10)
+            VStack(alignment: .leading) {
+                Text("Hello, SwiftUI!")
+                    .font(.title)
+                    .foregroundColor(.red)
+                HStack {
+                    Text("This is SwiftUI View")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Lesson 1")
+                }
+            }
+            Spacer()
+            
+            Button(action: { tapCount += 1 }) {
+                Text("Tap count: \(tapCount)")
+                    .font(.title)
             }
         }
         .padding()
