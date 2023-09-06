1a7af8a277f86c6e1efaf5e2749b13aa10570d71
Alexey Turulin <repakuku@icloud.com>
2023-09-06 14:21:49 -0400

    Initial Commit

---
+++ HelloWorldSwiftUI/HelloWorldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldSwiftUIApp.swift
+//  HelloWorldSwiftUI
+//
+//  Created by Alexey Turulin on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
